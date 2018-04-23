41c7b16b03557a5237e13c6df6637a1ced626362
QYG_XXY-PC\QYG_XXY <617659171@qq.com>
2018-04-23 18:44:43 +0800

    Initial commit

---
+++ app/src/main/java/jpush/qyq/com/weexsdk16project/MainActivity.java
@@ -0,0 +1,13 @@
+package jpush.qyq.com.weexsdk16project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
